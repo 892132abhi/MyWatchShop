9cd8342a4480fb695df7e825a671500ebcab3533
Abhiram <sabhiram41@gmail.com>
2026-04-22 12:20:03 +0530

    changed

--- Backend/products/Serializers.py
+++ Backend/products/Serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
 from .models import Products
+
 class ProductSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
     class Meta:
-        model=Products
-        fields='__all__'
-        
-    def get_image(self,obj):
-        try:
-            return obj.image.url
-        except Exception:
-            return None
+        model = Products
+        fields = '__all__'
